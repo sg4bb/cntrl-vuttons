ed84f557000ca8a7e68502a170575cdb052f0f3a
Gabriel <gaabrielserraa@gmail.com>
2023-05-31 15:11:34 -0400

    contact page modified

--- src/app/(routes)/contact/page.jsx
+++ src/app/(routes)/contact/page.jsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
+
+// ❤ assets
+import GabrielImg from "@/assets/images/meme/fornite-balls.jpg";
+import SergioImg from "@/assets/images/meme/muga-psp.gif";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <main className="grid place-content-center max-w-md mt-20">
+        <section className="flex space-x-14">
+          <div className="flex flex-col">
+            <Image src={GabrielImg} alt="pic 1 Contact temporally" />
+            <h1 className="text-1xl text-center text-black dark:text-white">
+              Gabo
+            </h1>
+          </div>
+          <div className="flex flex-col">
+            <Image src={SergioImg} alt="pic 2 Contact temporally" />
+            <h1 className="text-1xl text-center text-black dark:text-white">
+              Serginho
+            </h1>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Contact;
